24cbbaf5660c919774fbcd5bf5426d8edffbc098
bucknalla <alex.bucknall@gmail.com>
2018-02-16 17:54:14 +0000

    Added changes and test file

--- modulator.py
+++ modulator.py
@@ -1,4 +1,4 @@
-import os, sys, math
+import os, sys, math, string
 import numpy as np
 import commpy as cp
 import matplotlib.pyplot as plt
@@ -14,7 +14,6 @@ class modulator:
         self.scheme = scheme
         self.modulator = cp.modulation.QAMModem(self.scheme)
         self.draw = draw
-
         if self.draw:
             self.create_plot()
 
@@ -42,7 +41,7 @@ class modulator:
         plt.plot([data.real],[data.imag],'ro-',label='python')
 
     def draw_plot(self):
-        plt.show()
+        plt.draw()
 
     def open_bitstream(self, file):
         try:
@@ -50,22 +49,23 @@ class modulator:
             if(ext != ".bin"):
                 raise Exception
             bitstream = open(file, "r")
-            symbol_stream = open("temp","w") 
+            symbol_stream = open(".tmp","w") 
             for bits in self.read_bits(bitstream):
                 data = self.generate_symbols(bits)
-                self.load_symbol_stream(symbol_stream,str(data))
+                symbol_stream.write(str(data)+" ")
                 print(data)
                 if self.draw:
                     self.add_plot(data)
+            symbol_stream.close()
+            print(".tmp closed")
             if self.draw:
                 self.draw_plot()
-
         except OSError as err:
             print("OS error: {0}".format(err))
         except ValueError:
             print("Could not read data to binary format.")
-        # except Exception:
-        #     print("Incorrect file type.")
+        except Exception:
+            print("Incorrect file type.")
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
@@ -82,35 +82,95 @@ class modulator:
 
     def generate_symbols(self, bits):
         return self.modulator.modulate(bits)
-
-    def load_symbol_stream(self,file,data):
-        file.write(data)
         
 class serial_parallel:
-    def __init__(self, width=8):
+    def __init__(self, width=1):
         self.width = width
-        self.para_file = open("para", "w")
+        self.para_file = open(".par", "w")
+        print("Opening .tmp")
 
-    def parallelise_data(self,temp_file):
-        self.rw_symbols(temp_file, self.para_file)
+    def parallelise_data(self):
+        self.read_symbols()            
+        self.para_file.close()
+
+    def read_symbols(self):
+        cnt = 0
+        data = ""
+        newline = ""
+
+        with open(".tmp") as t:
+            for data in t.read():
+                if data == " ":
+                    cnt += 1
+                else:
+                    newline += data
+                if cnt >= self.width:
+                    self.para_file.write(str(newline)+"\n")
+                    newline = ""
+                    cnt = 0
+            t.close()
+
+class ifft():
+    def __init__(self,width=64):
+        self.width = width
+        self.para_file = open(".par", "r")
+
+
+    def ifft(self):
+        self.generate_array()
     
-    def rw_symbols(self, temp_file, para_file):
-        while True:
-            data = temp_file.read(self.width)
-            if not data:
-                break
-            para_file.write(data+"\n")
+    def generate_array(self):
+        ifft_store = []
+        ifft_mod_store = []
+        ifft_array = np.array()
+        # for symbol in range(0,self.width):
+        with open(".par", "r") as f:
+            for i, c in enumerate(f):
+                ifft_store.append(c)
+                ifft_mod_store.append("")
+                ifft_value = np.array(c.strip("\n"))
+                # print(ifft_value)
+                dot = False
+                for a, x in enumerate(ifft_store[i].strip("\n")):
+                    # print(ifft_store[i])
+                    if dot and x is "-":
+                        dot = False
+                    elif x is ".":
+                        dot = True
+                        x = x.replace('.', '')
+                    x = x.replace('[','')
+                    x = x.replace(']','')
+
+                    ifft_mod_store[i] += x
+                np.append(ifft_array,ifft_value)
+            print(ifft_array)
+
+
+            # for each in ifft_mod_store:
+            #     each = complex(each)
+            #     each = np.array([each])
+            #     # print(each)
+            #     np.append(ifft_array,each)
+
+            # print(element+"=")
+            # 
+                # print(ifft_array)
+
 
         
 mod = modulator()
-ser = serial_parallel()
 mod.open_bitstream("test.bin")
-# ser.parallelise_data("temp")
+ser = serial_parallel()
+ser.parallelise_data()
+ifft = ifft()
+ifft.ifft()
+
+plt.show()
 
 while True:
     if plt.get_fignums():
         pass
     else:
-        os.remove("temp")
-        os.remove("para")
+        # os.remove(".tmp")
+        # os.remove(".par")
         break
